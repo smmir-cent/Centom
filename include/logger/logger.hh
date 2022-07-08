06df9b034d954ecaa8bb94388206dac5ab51c079
smmir-cent <76453689+smmir-cent@users.noreply.github.com>
2022-07-08 12:13:37 +0000

    01/04/17 - 16:43 => fix logger.hh

--- include/logger/logger.hh
+++ include/logger/logger.hh
@@ -1,207 +1,91 @@
 #ifndef LOGGER
 #define LOGGER
-#include <filesystem>
-#include <memory>
-#include <sstream>
-#ifndef NOMINMAX
-#undef min
-#undef max
-#endif
-
-#include <spdlog/async.h>
-#include <spdlog/fmt/bundled/printf.h>
-#include <spdlog/fmt/fmt.h>
-#include <spdlog/logger.h>
-#include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/daily_file_sink.h>
-#include <spdlog/sinks/msvc_sink.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/sinks/stdout_sinks.h>
 #include <spdlog/spdlog.h>
 
-namespace fs = std::filesystem;
-/// spdlog wrap class
+#include <iostream>
 
-namespace wlog {
-class logger final {
- public:
-  /// let logger like stream
-  struct log_stream : public std::ostringstream {
-   public:
-    log_stream(const spdlog::source_loc& _loc, spdlog::level::level_enum _lvl,
-               std::string_view _prefix)
-        : loc(_loc), lvl(_lvl), prefix(_prefix) {}
+#include "spdlog/sinks/syslog_sink.h"
+class Logger {
+ private:
+  std::shared_ptr<spdlog::logger> syslog_logger;
 
-    ~log_stream() { flush(); }
-
-    void flush() { logger::get().log(loc, lvl, (prefix + str()).c_str()); }
-
-   private:
-    spdlog::source_loc loc;
-    spdlog::level::level_enum lvl = spdlog::level::info;
-    std::string prefix;
-  };
-
- public:
-  static logger& get() {
-    static logger logger;
-    return logger;
-  }
-
-  bool init(std::string_view log_file_path) {
-    if (_is_inited) return true;
+  Logger() {
+    // std::cout << "Called constructor of Logger" << std::endl;
     try {
-      // check log path and try to create log directory
-      fs::path log_path(log_file_path);
-      fs::path log_dir = log_path.parent_path();
-      if (!fs::exists(log_path)) {
-        fs::create_directories(log_dir);
-      }
-      // initialize spdlog
-      constexpr std::size_t log_buffer_size = 32 * 1024;  // 32kb
-      // constexpr std::size_t max_file_size = 50 * 1024 * 1024; // 50mb
-      spdlog::init_thread_pool(log_buffer_size,
-                               std::thread::hardware_concurrency());
-      std::vector<spdlog::sink_ptr> sinks;
-      auto daily_sink = std::make_shared<spdlog::sinks::daily_file_sink_mt>(
-          log_path.string(), 0, 2);
-      sinks.push_back(daily_sink);
-
-      // auto file_sink =
-      // std::make_shared<spdlog::sinks::basic_file_sink_mt>(log_path.string(),
-      // true); sinks.push_back(file_sink);
-
-#if defined(_DEBUG) && defined(WIN32)
-      auto ms_sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
-      sinks.push_back(ms_sink);
-#endif  //  _DEBUG
-
-#ifdef _LOG_CONSOLE
-      auto console_sink =
-          std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-      sinks.push_back(console_sink);
-#endif
-      spdlog::set_default_logger(
-          std::make_shared<spdlog::logger>("", sinks.begin(), sinks.end()));
-
-      spdlog::set_pattern("%s(%#): [%L %D %T.%e %P %t %!] %v");
-      spdlog::flush_on(spdlog::level::warn);
-      spdlog::set_level(_log_level);
-    } catch (std::exception_ptr e) {
-      assert(false);
-      return false;
+      this->syslog_logger =
+          spdlog::syslog_logger_st("syslog", "centom", LOG_PID);
+    } catch (const spdlog::spdlog_ex &ex) {
+      std::cout << "Log failed: " << ex.what() << std::endl;
+      exit(-1);
     }
-    _is_inited = true;
-    return true;
   }
 
-  void shutdown() { spdlog::shutdown(); }
+  Logger(Logger const &) = delete;
+  void operator=(Logger const &) = delete;
+
+ public:
+  // ~Logger() { std::cout << "Called destructor of Logger" << std::endl; }
+
+  static Logger &get_logger() {
+    static Logger new_loger;
+    return new_loger;
+  }
+
+  void setLevel(std::string level) {
+    if (level == "trace")
+      this->syslog_logger->set_level(spdlog::level::trace);
+    else if (level == "debug")
+      this->syslog_logger->set_level(spdlog::level::debug);
+    else if (level == "info")
+      this->syslog_logger->set_level(spdlog::level::info);
+    else if (level == "warn")
+      this->syslog_logger->set_level(spdlog::level::warn);
+    else if (level == "error")
+      this->syslog_logger->set_level(spdlog::level::err);
+    else if (level == "critical")
+      this->syslog_logger->set_level(spdlog::level::critical);
+    else
+      this->syslog_logger->set_level(spdlog::level::debug);
+  }
 
   template <typename... Args>
-  void log(const spdlog::source_loc& loc, spdlog::level::level_enum lvl,
-           const char* fmt, const Args&... args) {
-    spdlog::log(loc, lvl, fmt, args...);
+  void trace(const char *fmt, const Args... args) {
+    this->syslog_logger->trace(fmt, args...);
   }
 
   template <typename... Args>
-  void printf(const spdlog::source_loc& loc, spdlog::level::level_enum lvl,
-              const char* fmt, const Args&... args) {
-    spdlog::log(loc, lvl, fmt::sprintf(fmt, args...).c_str());
+  void debug(const char *fmt, const Args... args) {
+    this->syslog_logger->debug(fmt, args...);
   }
 
-  spdlog::level::level_enum level() { return _log_level; }
-
-  void set_level(spdlog::level::level_enum lvl) {
-    _log_level = lvl;
-    spdlog::set_level(lvl);
+  template <typename... Args>
+  void info(const char *fmt, const Args... args) {
+    this->syslog_logger->info(fmt, args...);
   }
 
-  void set_flush_on(spdlog::level::level_enum lvl) { spdlog::flush_on(lvl); }
-
-  static const char* get_shortname(std::string_view path) {
-    if (path.empty()) return path.data();
-
-    size_t pos = path.find_last_of("/\\");
-    return path.data() + ((pos == path.npos) ? 0 : pos + 1);
+  template <typename... Args>
+  void warn(const char *fmt, const Args... args) {
+    this->syslog_logger->warn(fmt, args...);
   }
 
- private:
-  logger() = default;
-  ~logger() = default;
+  template <typename... Args>
+  void error(const char *fmt, const Args... args) {
+    this->syslog_logger->error(fmt, args...);
+  }
 
-  logger(const logger&) = delete;
-  void operator=(const logger&) = delete;
-
- private:
-  std::atomic_bool _is_inited = false;
-  spdlog::level::level_enum _log_level = spdlog::level::trace;
+  template <typename... Args>
+  void critical(const char *fmt, const Args... args) {
+    this->syslog_logger->critical(fmt, args...);
+  }
 };
-}  // namespace wlog
 
-// got short filename(exlude file directory)
-#define __FILENAME__ (wlog::logger::get_shortname(__FILE__))
+Logger &logger = Logger::get_logger();
 
-// use fmt lib, e.g. LOG_WARN("warn log, {1}, {1}, {2}", 1, 2);
-#define LOG_TRACE(msg, ...)                                  \
-  {                                                          \
-    if (wlog::logger::get().level() == spdlog::level::trace) \
-      spdlog::log({__FILENAME__, __LINE__, __FUNCTION__},    \
-                  spdlog::level::trace, msg, ##__VA_ARGS__); \
-  };
-#define LOG_DEBUG(msg, ...)                                                 \
-  spdlog::log({__FILENAME__, __LINE__, __FUNCTION__}, spdlog::level::debug, \
-              msg, ##__VA_ARGS__)
-#define LOG_INFO(msg, ...)                                                 \
-  spdlog::log({__FILENAME__, __LINE__, __FUNCTION__}, spdlog::level::info, \
-              msg, ##__VA_ARGS__)
-#define LOG_WARN(msg, ...)                                                 \
-  spdlog::log({__FILENAME__, __LINE__, __FUNCTION__}, spdlog::level::warn, \
-              msg, ##__VA_ARGS__)
-#define LOG_ERROR(msg, ...)                                                    \
-  spdlog::log({__FILENAME__, __LINE__, __FUNCTION__}, spdlog::level::err, msg, \
-              ##__VA_ARGS__)
-#define LOG_FATAL(msg, ...)                                                    \
-  spdlog::log({__FILENAME__, __LINE__, __FUNCTION__}, spdlog::level::critical, \
-              msg, ##__VA_ARGS__)
-
-// use like sprintf, e.g. PRINT_WARN("warn log, %d-%d", 1, 2);
-#define PRINT_TRACE(msg, ...)                                        \
-  wlog::logger::get().printf({__FILENAME__, __LINE__, __FUNCTION__}, \
-                             spdlog::level::trace, msg, ##__VA_ARGS__);
-#define PRINT_DEBUG(msg, ...)                                        \
-  wlog::logger::get().printf({__FILENAME__, __LINE__, __FUNCTION__}, \
-                             spdlog::level::debug, msg, ##__VA_ARGS__);
-#define PRINT_INFO(msg, ...)                                         \
-  wlog::logger::get().printf({__FILENAME__, __LINE__, __FUNCTION__}, \
-                             spdlog::level::info, msg, ##__VA_ARGS__);
-#define PRINT_WARN(msg, ...)                                         \
-  wlog::logger::get().printf({__FILENAME__, __LINE__, __FUNCTION__}, \
-                             spdlog::level::warn, msg, ##__VA_ARGS__);
-#define PRINT_ERROR(msg, ...)                                        \
-  wlog::logger::get().printf({__FILENAME__, __LINE__, __FUNCTION__}, \
-                             spdlog::level::err, msg, ##__VA_ARGS__);
-#define PRINT_FATAL(msg, ...)                                        \
-  wlog::logger::get().printf({__FILENAME__, __LINE__, __FUNCTION__}, \
-                             spdlog::level::critical, msg, ##__VA_ARGS__);
-
-// use like stream , e.g. STM_WARN() << "warn log: " << 1;
-#define STM_TRACE()                                                \
-  wlog::logger::log_stream({__FILENAME__, __LINE__, __FUNCTION__}, \
-                           spdlog::level::trace, "")
-#define STM_DEBUG()                                                \
-  wlog::logger::log_stream({__FILENAME__, __LINE__, __FUNCTION__}, \
-                           spdlog::level::debug, "")
-#define STM_INFO()                                                 \
-  wlog::logger::log_stream({__FILENAME__, __LINE__, __FUNCTION__}, \
-                           spdlog::level::info, "")
-#define STM_WARN()                                                 \
-  wlog::logger::log_stream({__FILENAME__, __LINE__, __FUNCTION__}, \
-                           spdlog::level::warn, "")
-#define STM_ERROR()                                                \
-  wlog::logger::log_stream({__FILENAME__, __LINE__, __FUNCTION__}, \
-                           spdlog::level::err, "")
-#define STM_FATAL()                                                \
-  wlog::logger::log_stream({__FILENAME__, __LINE__, __FUNCTION__}, \
-                           spdlog::level::critical, "")
+#define TRACE(...) logger.trace(__VA_ARGS__)
+#define DEBUG(...) logger.debug(__VA_ARGS__)
+#define INFO(...) logger.info(__VA_ARGS__)
+#define WARN(...) logger.warn(__VA_ARGS__)
+#define ERROR(...) logger.error(__VA_ARGS__)
+#define CRITICAL(...) logger.critical(__VA_ARGS__)
 
 #endif
--- src/centom_engine.cc
+++ src/centom_engine.cc
@@ -1,42 +1,26 @@
 
 #include <iostream>
 
+#include "logger/logger.hh"
 #include "rapidjson/document.h"
 #include "rapidjson/filereadstream.h"
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/writer.h"
 #include "spdlog/sinks/syslog_sink.h"
 #include "spdlog/spdlog.h"
-
 namespace rj = rapidjson;
+extern Logger& logger;
+
+void spdlogTest() {
+  TRACE("TRACE");
+  DEBUG("DEBUG");
+  INFO("INFO");
+  WARN("WARN");
+  ERROR("ERROR");
+  CRITICAL("CRITICAL");
+}
 
 int main(int argc, char** argv) {
-  // test spdlog
-  spdlog::info("Welcome to spdlog!");
-  spdlog::error("Some error message with arg: {}", 1);
-
-  spdlog::warn("Easy padding in numbers like {:08d}", 12);
-  spdlog::critical(
-      "Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-  spdlog::info("Support for floats {:03.2f}", 1.23456);
-  spdlog::info("Positional args are {1} {0}..", "too", "supported");
-  spdlog::info("{:<30}", "left aligned");
-
-  spdlog::set_level(spdlog::level::debug);  // Set global log level to debug
-  spdlog::debug("This message should be displayed..");
-
-  // change log pattern
-  spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
-
-  // Compile time log levels
-  // define SPDLOG_ACTIVE_LEVEL to desired level
-  SPDLOG_TRACE("Some trace message with param {}", 42);
-  SPDLOG_DEBUG("Some debug message");
-
-  std::string ident = "centom";
-  auto syslog_logger = spdlog::syslog_logger_mt("syslog", ident, LOG_PID);
-  syslog_logger->warn("This is warning that will end up in syslog.");
-
   FILE* fp = fopen("/etc/centom/centom.json", "r");
 
   char readBuffer[65536];
@@ -51,4 +35,6 @@ int main(int argc, char** argv) {
   std::cout << "snmp_version: " << document["snmp_version"].GetInt()
             << std::endl;
   fclose(fp);
+  logger.setLevel(document["log_level"].GetString());
+  spdlogTest();
 }
