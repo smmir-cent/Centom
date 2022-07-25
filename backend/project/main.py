bb6f162a94fb378df475347002d55bdfb5fb736d
smmir-cent <76453689+smmir-cent@users.noreply.github.com>
2022-07-25 04:15:02 -0400

    01/05/03 - 12:45 => ready for merge with snmp_dev

--- backend/project/main.py
+++ backend/project/main.py
@@ -1,6 +1,6 @@
 # main.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request
 from flask_login import login_required, current_user
 
 main = Blueprint('main', __name__)
@@ -18,3 +18,15 @@ def test():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name, surname=current_user.surname, mobile_number=current_user.mobile_number, email=current_user.email)
+
+@main.route('/test', methods=['POST'])
+def testScan():
+    print(request.form["ip"])
+    selected = request.form.getlist('c_check')
+    print(selected)
+    any_selected = bool(selected)
+    print(any_selected)
+    ## todo run snmp based on form's inputs
+    
+    return render_template('index.html')
+
