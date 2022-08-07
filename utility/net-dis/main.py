8e3ad7e24093aaeb12dd8686bba349a858042579
smmir-cent <76453689+smmir-cent@users.noreply.github.com>
2022-08-07 12:15:17 -0400

    01/05/16 - 20:44 => excule all 0 or 1

--- utility/net-dis/main.py
+++ utility/net-dis/main.py
@@ -28,21 +28,27 @@ if __name__ == "__main__":
 
     ip_validation(sys.argv[1])
     targets = []
+    mask = int(sys.argv[1].split('/',1)[1])
+
     for ip in ipaddress.IPv4Network(sys.argv[1]):
         # print(ip)
-        args = ['../build/centom_engine','-systest']
-        args.extend([str(ip)])
-        # print(args)
-        engine_result = subprocess.run(args, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        result_out = engine_result.stdout.decode('utf-8')
-        result_err = engine_result.stderr.decode('utf-8')
+        bin_ip = ('.'.join([bin(int(x)+256)[3:] for x in str(ip).split('.')])).replace('.','')
+        host_bits = bin_ip[mask-32:]
+        if '0' in host_bits and '1' in host_bits:
+            args = ['../build/centom_engine','-systest']
+            args.extend([str(ip)])
+            engine_result = subprocess.run(args, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            result_out = engine_result.stdout.decode('utf-8')
+            result_err = engine_result.stderr.decode('utf-8')
 
-        if "Timeout".lower() in result_out.lower() or "Timeout".lower() in result_err.lower():
-            print(f'{str(ip)} is dead')
-            pass
-        else:
-            print(f'{str(ip)} is alive')
-            targets.append(ip)
+            if "Timeout".lower() in result_out.lower() or "Timeout".lower() in result_err.lower():
+                print(f'{str(ip)} is dead')
+                pass
+            else:
+                print(f'{str(ip)} is alive')
+                targets.append(ip)
     
-    # print("alive servers/switches/routers")
-    # print(targets)
+    print("alive servers/switches/routers")
+    print(targets)
+
+
